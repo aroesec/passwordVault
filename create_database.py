531a4bab947daadd54949bb58afa7f0690bd9548
aroe <aroe3sec@gmail.com>
2020-09-30 21:14:40 -0600

    added database function, added user/password logic

--- create_database.py
+++ create_database.py
@@ -1,13 +1,28 @@
-import psycopg2
+import psycopg2 ##module won't import
 import sys
 from encryption import hash_me
 
 #Does hash_me store input or does it just get called? Need it to store input
 
+user = input('Please enter a username to store into the databse')
+
+hash_me() ##gives password from the encryption file
+
 def create_tables():
-    con = None
-    con = psycopg2.connect("host='localhost' dbname='os.environ(NAME)' user='aroe' password=os.envion(PASSWORD)")   
+    conn = psycopg2.connect("host='localhost' dbname='os.environ(NAME)' user='aroe' password=os.envion(PASSWORD)")
+       
     cur = con.cursor()
+    cur.execute("CREATE TABLE users (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.execute(f"INSERT INTO users({user}, {password}) VALUES(%s, %s")
+
+
+
+def queryTable():
+    ##example query below
+    cure.execute('SELECT * FROM users;') ##in-line query
+    cur.fetchone()
+
+    ##user and password variables need to be input in CLI
 #Create tables below
 
 
--- encryption.py
+++ encryption.py
@@ -4,17 +4,10 @@ def hash_me():
     raw = hashlib.new('sha256')   #declares new hash 
     clientPass = input("enter your password to be hashed: ") #updates declared hash with password
     byteTransform = bytes(q, 'utf-8')
-    raw.update(w) #this is stringing q instead of insert q as stored input TODO FIX
+    raw.update(raw) #this is stringing q instead of insert q as stored input TODO FIX
     close = raw.hexdigest()  #digests and hashes password
-    
-    
+return password
+##This program should output the password and then the hashed password should be imported into the create_database file
 
-#TODO convert var a back to bytes to then decrypt to test
-    #print(q)
-    # attempting to decrypt the sha256 for testing
-    # fernet = Fernet(a)   
-    # print(type(a))
-    # encrypted = fernet.decrypt(a)
-    # print(a)
 
 hash_me()
